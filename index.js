5b6e2ee13912b9d9fab52f90e97412f974925e8e
ibrahimkhalilullahshafi <shafi.warx@gmail.com>
2022-11-27 19:49:44 +0600

    categories data added & get

--- index.js
+++ index.js
@@ -3,14 +3,40 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(express.json());
 app.use(cors());
 
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.eaa9w7x.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        const categoriesCollection = client.db('foneSell').collection('categories');
+        const productsCollection = client.db('foneSell').collection('products');
+
+
+        app.get('/categories', async (req, res) => {
+            const query = {}
+            const cursor = categoriesCollection.find(query);
+            const categories = await cursor.toArray();
+            res.send(categories);
+        });
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.error(error));
+
+
+
+
 app.get("/", (req, res) => {
-    res.send("server is runnnnnning");
+    res.send("server is runing");
 });
 
 
